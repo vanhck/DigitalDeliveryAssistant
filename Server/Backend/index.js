0f71e7c2ef808be69a88f361f7597e8e3ce14326
Dimitrios Makridis <dimitrios.makridis@sap.com>
2017-06-24 01:48:10 +0200

    added endpoint for jonas

--- Server/Backend/index.js
+++ Server/Backend/index.js
@@ -6,6 +6,16 @@ var googleMapsClient = require('@google/maps').createClient({
 
 const app = express();
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "hackvans"
+});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Database connected.");
+});
+
 app.get('/', function (req, res) {
 	googleMapsClient.distanceMatrix(
 		{
@@ -18,12 +28,31 @@ app.get('/', function (req, res) {
 		function(results, response) {
 			console.log(results);
 			res.send(response.json.rows);
-			
 			console.log(response.json.rows[0].elements[0]);
 		}
 	);
 })
 
+
+	
+app.get('/customerData', function (req, res) {
+	//res.send(req.params.customerId);
+	callback = function(response){
+		res.send(response);
+	}
+	con.query("SELECT * FROM empfeanger", function (err, result) {
+		var resData = {};
+		resData.id = result[0].ID;
+		resData.vorname = result[0].NAME;
+		resData.nachname = result[0].NACHNAME;
+		resData.coins = 100;
+		resData.deliverys = 3;
+		resData.pickUp_person = {"lat":80.12304846, "lng": 7.123984211};
+		resData.car_parking = {"lat":82.14104846, "lng":  8.100984211};
+		callback(resData);
+	})
+})
+
 app.listen(3000, function () {
-  console.log('Server listening on port 80!')
+  console.log('Server listening on port 3000!')
 })
