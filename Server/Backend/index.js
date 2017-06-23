02cab4df4fec3d8a5a1ded8d7d075ba0c8de4477
Dimitrios Makridis <dimitrios.makridis@sap.com>
2017-06-24 00:38:51 +0200

    added backend

---
+++ Server/Backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+var mysql = require('mysql');
+var googleMapsClient = require('@google/maps').createClient({
+  key:'AIzaSyDvKst18hM40EO98NeMeEFNxRnbegx_kQI'
+});
+
+const app = express();
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.get('/', function (req, res) {
+	googleMapsClient.distanceMatrix(
+		{
+			origins: [[-33.7032647,151.0962581], 'Chatswood Station, NSW'],
+			destinations: ['Central Station, NSW', 'Parramatta Station, NSW'],
+			language: 'en',
+			units: 'metric',
+			region: 'au'
+		},
+		function(results, response) {
+			console.log(results);
+			res.send(response.json.rows);
+			
+			console.log(response.json.rows[0].elements[0]);
+		}
+	);
+})
+
+app.listen(80, function () {
+  console.log('Server listening on port 80!')
+})
