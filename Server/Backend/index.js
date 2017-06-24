10a0a34bb753c8b579c43b256fc0790017678c87
Dimitrios Makridis <dimitrios.makridis@sap.com>
2017-06-24 09:46:04 +0200

    Realtime data feed update endpoint driverStatus

--- Server/Backend/index.js
+++ Server/Backend/index.js
@@ -54,22 +54,18 @@ app.get('/customerData', function (req, res) {
 	})
 })
 
-app.get('/driverStatus', function (req, res) {
+app.get('/driverStatus/:lat?/:lng?', function (req, res) {
 	//res.send(req.params.customerId);
 	console.log("driver status called.");
-	curLat = 80.12304846;
-	curLng = 7.123984211;
+	
+	curLat = (req.params.lat)? req.params.lat: 80.12304846;
+	curLng = (req.params.lng)? req.params.lng: 7.123984211;
 	callback = function(response){
 		res.send(response);
 	}
 	calculateDestinations = function(destinations){
-		/*arrayDestinations = [];
-		for(var o = 0; o < destinations.length; o++){
-			arrayDestinations.push([
-				destinations[o].lat,
-				destinations[o].lng,
-			])
-		}*/
+		//destinations.push("Wiesloch Albert-Schweitzer-Schtrasse 02");
+		console.log(destinations);
 		googleMapsClient.distanceMatrix(
 			{
 				origins: destinations,
@@ -109,16 +105,16 @@ app.get('/driverStatus', function (req, res) {
 					return p(array, 0, callback);
 				  }
 				  
-				var possibilities = [
-					{"i":0,"description":response.json.destination_addresses[0]},
-					{"i":1,"description":response.json.destination_addresses[1]},
-					{"i":2,"description":response.json.destination_addresses[2]}
-				]
+			var possibilities = [];
+			for(var i = 0; i < response.json.destination_addresses.length; i++){
+				possibilities.push({"i":i,"description":response.json.destination_addresses[i]});
+			}
+				
 				kombiX = [];
 				for(var x = 0; x < response.json.origin_addresses.length; x++){
 					kombiY = [];
 					for(var y = 0; y < response.json.destination_addresses.length; y++){
-						kombiY.push(response.json.rows[x].elements[y].duration.value);
+						kombiY.push({"score":response.json.rows[x].elements[y].duration.value,"time":response.json.rows[x].elements[y].duration.text,"distance":response.json.rows[x].elements[y].distance.text});
 					}
 					kombiX.push(kombiY);
 				}
@@ -130,26 +126,52 @@ app.get('/driverStatus', function (req, res) {
 					result.push(a.slice(0));
 				  });
 				
-				var best = {score:-1, points:[]};
+				var best = {score:-1, points:[], distance:"", time:""};
 				for(var i = 0; i < result.length; i++){
 					
 					if(best.score == -1){
 						best.points = result[i];
-						best.score = kombiX[result[i][0].i][result[i][1].i] + kombiX[result[i][1].i][result[i][2].i] ;
-					}else if(kombiX[result[i][0].i][result[i][1].i] + kombiX[result[i][1].i][result[i][2].i]<best.score){
-						best.points = result[i];
-						best.score = kombiX[result[i][0].i][result[i][1].i] + kombiX[result[i][1].i][result[i][2].i]
+						best.score = 0;
+						for(var j = 0; j < result[i].length-1; j++){
+							best.score += kombiX[result[i][0+j].i][result[i][1+j].i].score;
+							best.points[j].time = kombiX[result[i][0+j].i][result[i][1+j].i].time;
+							best.points[j].distance = kombiX[result[i][0+j].i][result[i][1+j].i].distance;
+							best.points[j].socre = kombiX[result[i][0+j].i][result[i][1+j].i].score;
+						}
+					}else{
+						var concScore = 0;
+						for(var j = 0; j < result[i].length-1; j++){
+							concScore += kombiX[result[i][0+j].i][result[i][1+j].i].score;
+							
+						}
+						if(concScore<best.score){
+							best.points = result[i];
+							best.score = concScore;
+							for(var j = 0; j < best.points.length-1; j++){
+								best.points[j].time = kombiX[result[i][0+j].i][result[i][1+j].i].time;
+								best.points[j].distance = kombiX[result[i][0+j].i][result[i][1+j].i].distance;
+								best.points[j].score = kombiX[result[i][0+j].i][result[i][1+j].i].score;
+							}
+						}
+						
+						
 					}
 				}
 				
 				//callback({"a":kombiX,"b": result, "c": best, "result[i][0].i":result[0][0].i ,"result[i][1].i":result[0][1].i});
 				var destinations_fastest = [];
+				var date = new Date();
+				var timeStamp = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + "T" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 				for(var i = 0; i < best.points.length;i++){
+					var futureDate = new Date(Date.now() + (1000*best.points[i].score%60 /*sec*/ * 60*best.points[i].score/60 /*min*/ * 60*best.points[i].score/3600))
 					destinations_fastest.push({
+						"feasibilityScore":best.score,
 						"description":best.points[i].description,
+						"time" : best.points[i].time,
+						"distance" : best.points[i].distance,
 						"arival_time":"",
-						"estimated_arrival_time":"2017-06-24:" +(11*i) + ":42:12",
-						"appraised_arrivalTime":"2017-06-24:" +(12*i) +":45:12"
+						"estimated_arrival_time":best.points[i].score? futureDate.getFullYear() + "-" + (futureDate.getMonth()+1) + "-" + futureDate.getDate() + "T" + futureDate.getHours() + ":" + futureDate.getMinutes() + ":" + date.getSeconds() : "",
+						"appraised_arrivalTime":"2017-06-24T" +(12*i) +":45:12"
 						});
 				}
 				callback(destinations_fastest);
@@ -176,7 +198,6 @@ app.get('/driverStatus', function (req, res) {
 			});
 			destinations.push(result[i].WOHNORT + " " + result[i].STRASSE+ " " + result[i].HAUS_NR);
 		}
-		console.log(destinations);
 		calculateDestinations(destinations);
 	})
 })
